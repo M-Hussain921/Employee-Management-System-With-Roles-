c4e52315908b0d45541985751b10efe0cf4a57ca
Hussain <hussainweb2026@gmail.com>
2026-03-19 20:52:32 +0530

    add public route

--- router/departmantRouter.js
+++ router/departmantRouter.js
@@ -5,6 +5,7 @@ const {authMiddleware,isAdminOrhr}=require('../middleware/authMiddleware');
 const {createDepartment,getDepartment,getDepbyId,searchBydepartment,deleteDepartment,updateDepartment}=require('../controller/departmentController');
 
 router.get('/all-department',authMiddleware,isAdminOrhr,getDepartment);
+router.get('/public-department',getDepartment);
 router.post('/create-department',authMiddleware,isAdminOrhr,createDepartment);
 router.get('/:id',authMiddleware,isAdminOrhr,getDepbyId);
 router.get('/', authMiddleware, isAdminOrhr, searchBydepartment);
