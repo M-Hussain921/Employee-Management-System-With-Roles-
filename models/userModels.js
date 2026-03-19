1ab57262ec900977384457b461a9458cf3eab942
Hussain <hussainweb2026@gmail.com>
2026-03-19 13:25:43 +0530

    improve user model

--- models/userModels.js
+++ models/userModels.js
@@ -1,13 +1,45 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    contactNumber: { type: Number },
-    age: { type: Number },
-    role: { type: String, required: true, enum: ["admin", "hr", "employee"], default: "employee" },
-    department: { type: mongoose.Schema.Types.ObjectId, ref: "Department",required:false }
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    contactNumber: { 
+        type: Number,
+        required:true
+     },
+
+    age: { 
+        type: Number,
+    required:true
+    },
+
+    role: {
+        type: String,
+        required: true,
+        enum: ["super_admin","admin", "hr", "employee"],
+        default: "employee"
+    },
+
+    department: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Department",
+        required: false
+    }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('users', userSchema);
