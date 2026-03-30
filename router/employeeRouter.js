fbcc45e2b00d0a3e0709fbe8e6bff66e95f0f3bd
Hussain <hussainweb2026@gmail.com>
2026-03-30 11:26:00 +0530

    Set Static and Dynamic routes

--- router/employeeRouter.js
+++ router/employeeRouter.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const { authMiddleware, isAdmin, isAdminOrhr } = require('../middleware/authMiddleware');
 const { deleteUser,  updateEmp, getemployeebyid, getallemployee, createEmployee, searchByrole,filterbyage,totalemployees,empPerDepartment,empPerRole,updateUser,deleteEmployee, getUserbyId} = require('../controller/employeeController');
 
-router.get('/:id',authMiddleware,getUserbyId)
 router.post('/create-employee', authMiddleware, isAdminOrhr, createEmployee);
 router.get('/all-employee', authMiddleware,getallemployee);
-router.get('/employee/:id', authMiddleware, getemployeebyid);
 router.get('/employee-role', authMiddleware, isAdminOrhr, searchByrole);
 router.get('/employee-age',authMiddleware,isAdminOrhr,filterbyage);
 router.get('/total-employees',authMiddleware,isAdminOrhr,totalemployees);
 router.get('/employees-per-department',authMiddleware,isAdminOrhr,empPerDepartment);
 router.get('/employees-per-role',authMiddleware,isAdminOrhr,empPerRole);
+router.get('/:id',authMiddleware,getUserbyId);
+router.get('/employee/:id', authMiddleware, getemployeebyid);
 router.put('/update-employee/:id', authMiddleware, isAdminOrhr, updateEmp);
 router.put('/update/:id', authMiddleware,updateUser);
 router.delete("/delete-employee/:id", authMiddleware, isAdmin, deleteEmployee);
